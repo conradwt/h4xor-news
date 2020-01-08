d4b7bf869c4ba621ed0a43a9dfdab3fa1095af41
Conrad Taylor <conradwt@gmail.com>
2020-01-08 13:18:58 -0800

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Conrad Taylor on 1/8/20.
+//  Copyright © 2020 Conrad Taylor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
